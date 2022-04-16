b7d97a32c31e88662126912dbedb3c8e7f8aa2b1
vasha54 <91905466+vasha54@users.noreply.github.com>
2022-04-15 22:45:01 -0400

    Add files via upload

---
+++ DMOJ - Bender y los Números Aleatorios.java
@@ -0,0 +1,73 @@
+/*
+DMOJ - Bender y los Números Aleatorios
+Tematica: Add Hoc +String
+
+Idea Basica: Implementación de lo que propone el problema
+*/
+import java.io.*;
+import java.math.*;
+import java.util.*;
+import java.util.*;
+import java.lang.*;
+import java.util.regex.*;
+
+
+public class Main {
+    
+    private BufferedReader in;
+    private PrintWriter out;
+    private StringTokenizer st;
+    private final int MAX_N = 1000001;
+    private final int MAX_TREE = (MAX_N << 2);
+    
+    private int [] movX = { 0, 0, 1, -1};
+    private int [] movY = {-1, 1, 0,  0};
+    private long [] cards ={1,10,100,1000,10000};
+    private long MOD =1000000009;
+    
+   
+   
+   
+    public void solve() throws Exception {
+        long n =nextLong();
+        n*=n;
+        String p = String.valueOf(n);
+        p = p.length()==8? p.substring(2,6) :p.substring(1,5);
+        out.printf("%d\n",Long.parseLong(p));
+    }
+    
+    Main() throws Exception {
+       /*Esta entrada  se debe activar cuando se hace una ejercicio de lectura hasta fin de fichero 
+       copiar la entrada en un fichero "Input.txt" y comentar el otro in . A la hor de enviar comentar
+       este y descomentar el otro
+       */
+       //in = new BufferedReader(new FileReader("Input.txt"));
+       //out = new PrintWriter(new FileWriter("Output.txt"));
+       in = new BufferedReader(new InputStreamReader(System.in));
+	   out = new PrintWriter(System.out);
+       eat("");
+       solve();
+       in.close();
+       out.close();
+    }
+
+    private void eat(String str) { st = new StringTokenizer(str);}
+
+    String next() throws Exception {
+        while (!st.hasMoreTokens()) {
+            String line = in.readLine();
+            if (line == null) return null;
+            eat(line);
+        }
+        return st.nextToken();
+    }
+
+    int nextInt() throws Exception {return Integer.parseInt(next());}
+
+    long nextLong() throws Exception {return Long.parseLong(next());}
+
+    double nextDouble() throws Exception {return Double.parseDouble(next());}
+
+    public static void main(String[] args) throws Exception {new Main();}
+
+}
