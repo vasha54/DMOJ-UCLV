f669dcc76e634bb04582979f882115a55bb543e6
Luis Andrés Valido Fajardo <91905466+vasha54@users.noreply.github.com>
2023-02-23 20:47:54 -0500

    Add files via upload

---
+++ DMOJ - Enchufes Eléctricos.java
@@ -0,0 +1,92 @@
+/**
+ * DMOJ - Enchufes Eléctricos
+ * Temáticas: Greddy 
+ *  
+ * Idea: Sumamos todos los valores y restamos la cantidad de regletas menos uno
+ *    
+ * . 
+ */
+import java.io.*;
+import java.math.*;
+import java.util.*;
+import java.util.*;
+import java.lang.*;
+import java.util.regex.*;
+
+public class Main {
+
+	private BufferedReader in;
+	private PrintWriter out;
+	private StringTokenizer st;
+	private final int MAX_N = 110;
+	private final int MAX_TREE = (MAX_N << 2);
+	private final char SYMBOL = '@';
+
+	private int[] movX = { 0, 0, 1, -1 };
+	private int[] movY = { -1, 1, 0, 0 };
+	private long[] cards = { 1, 10, 100, 1000, 10000 };
+	private long MOD = 1000000009;
+
+
+	public void solve() throws Exception {
+		int cases =nextInt(),regletas,orificios,suma;
+		while(cases > 0) {
+			cases --;
+			suma=0;
+			regletas=nextInt();
+			for(int i=0;i<regletas;i++) {
+				orificios =nextInt();
+				suma+=orificios;
+			}
+			out.printf("%d\n", suma-(regletas-1));
+		}
+	}
+
+
+	Main() throws Exception {
+		/*
+		 * Esta entrada se debe activar cuando se hace una ejercicio de lectura hasta
+		 * fin de fichero copiar la entrada en un fichero "Input.txt" y comentar el otro
+		 * in . A la hor de enviar comentar este y descomentar el otro
+		 */
+		// in = new BufferedReader(new FileReader("Inpu.txt"));
+		// out = new PrintWriter(new FileWriter("Output.txt"));
+		in = new BufferedReader(new InputStreamReader(System.in));
+		out = new PrintWriter(System.out);
+		eat("");
+		solve();
+		in.close();
+		out.close();
+	}
+
+	private void eat(String str) {
+		st = new StringTokenizer(str);
+	}
+
+	String next() throws Exception {
+		while (!st.hasMoreTokens()) {
+			String line = in.readLine();
+			if (line == null)
+				return null;
+			eat(line);
+		}
+		return st.nextToken();
+	}
+
+	int nextInt() throws Exception {
+		return Integer.parseInt(next());
+	}
+
+	long nextLong() throws Exception {
+		return Long.parseLong(next());
+	}
+
+	double nextDouble() throws Exception {
+		return Double.parseDouble(next());
+	}
+
+	public static void main(String[] args) throws Exception {
+		new Main();
+	}
+
+}
